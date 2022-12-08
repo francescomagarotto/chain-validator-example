eede04061257efc78faf8ad39d6e341685899a37
francescomagarotto <magarotto.francesco@gmail.com>
2022-12-08 10:49:31 +0100

    WeakHashMap for errors

--- src/main/java/com/francescomagarotto/chainvalidator/Main.java
+++ src/main/java/com/francescomagarotto/chainvalidator/Main.java
@@ -10,7 +10,7 @@ public class Main {
         ValidatorChain<Pippo> chain = ValidatorChain.of(pippo)
                 .chain((String s) -> true, Pippo::getName)
                 .chain((String s) -> s.length() == 7, Pippo::getName)
-                .chain((String s) -> s.length() == 2, Pippo::getSurname)
+                .chain((String s) -> s.length() == 2, Pippo::getSurname, "Surname length should be 7")
                 .buildChain();
         System.out.println(chain.validate());
     }
--- src/main/java/com/francescomagarotto/chainvalidator/ValidatorChain.java
+++ src/main/java/com/francescomagarotto/chainvalidator/ValidatorChain.java
@@ -1,18 +1,25 @@
 package com.francescomagarotto.chainvalidator;
 
+import org.jetbrains.annotations.NotNull;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.WeakHashMap;
 import java.util.function.Predicate;
 
 @SuppressWarnings({"rawtypes", "unchecked"})
 public class ValidatorChain<E> {
-
-    private LinkedList<Pair<Predicate, Transform>> couples;
+    private final static Logger LOGGER = LoggerFactory.getLogger(ValidatorChain.class);
+    private final LinkedList<Pair<Predicate, Transform>> couples;
+    private final WeakHashMap<Pair<Predicate, Transform>, String> errors;
     private final E entity;
 
-    private ValidatorChain(E entity, LinkedList<Pair<Predicate, Transform>> couples) {
+    private ValidatorChain(E entity, LinkedList<Pair<Predicate, Transform>> couples, WeakHashMap<Pair<Predicate, Transform>, String> errors) {
         this.entity = entity;
         this.couples = couples;
+        this.errors = errors;
     }
 
     public static <E> Chain<E> of(E entity) {
@@ -23,20 +30,25 @@ public class ValidatorChain<E> {
         boolean valid = true;
         Iterator<Pair<Predicate, Transform>> pairIterator = couples.iterator();
         Pair<Predicate, Transform> pair;
-        while(pairIterator.hasNext() && valid) {
+        while (pairIterator.hasNext() && valid) {
             pair = pairIterator.next();
             valid = pair.l.test(pair.r.transform(entity));
+            if (!valid) {
+                LOGGER.error(errors.get(pair));
+            }
         }
         return valid;
     }
 
     public static class Chain<E> {
         private final LinkedList<Pair<Predicate, Transform>> couples;
+        private final WeakHashMap<Pair<Predicate, Transform>, String> errors;
         private final E entity;
 
         public Chain(E entity) {
             this.couples = new LinkedList<>();
             this.entity = entity;
+            this.errors = new WeakHashMap<>();
         }
 
         public <O> Chain<E> chain(Predicate<O> predicate, Transform<E, O> transform) {
@@ -44,8 +56,15 @@ public class ValidatorChain<E> {
             return this;
         }
 
+        public <O> Chain<E> chain(Predicate<O> predicate, Transform<E, O> transform, @NotNull String errorMessage) {
+            Pair p = new Pair<>(predicate, transform);
+            couples.add(p);
+            errors.put(p, errorMessage);
+            return this;
+        }
+
         public ValidatorChain<E> buildChain() {
-            return new ValidatorChain<>(entity, couples);
+            return new ValidatorChain<>(entity, couples, errors);
         }
     }
 
